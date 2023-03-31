3ea589982f60955e538cc2015d68bf3d7f31e515
“JoaquinMeneses” <joakinmeneses.jm@gmail.com>
2023-03-31 09:49:06 -0300

    task2-v1

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+// Trabajo con innerHTML
+const contenedor = document.getElementById("eventos")
+const eventos = data.eventos
+const evento = eventos
+let card = ``
+
+function agregarEventoHtml(evento) {
+    let alt = evento.name.replace(` `,`-`)
+    return `<article class="card bg-black col-10 col-md-3 rounded-5">
+                <img class="h-50 p-2 rounded-5" src="${evento.image}" class="card-img-top" alt="${alt}">
+                <div class="d-flex justify-content-between flex-column card-body">
+                    <h5 class="card-title fw-bold text-center">${evento.name}</h5>
+                    <p class="card-text">${evento.description}</p>
+                    <div class="d-flex justify-content-between align-items-baseline">
+                        <p class="card-text fw-bold">$${evento.price}</p>
+                        <a class="btn-card fw-bold" href="./assets/pages/details.html">Ver más...</a>    
+                    </div>
+                </div>
+            </article>`
+}
+
+for (let evento of eventos) {
+    card += agregarEventoHtml(evento)
+}
+contenedor.innerHTML = card;
+
+/* Plantilla de card
+<article class="card bg-black col-10 col-md-3 rounded-5">
+    <img class="h-50 p-2 rounded-5" src="" class="card-img-top" alt="">
+    <div class="d-flex justify-content-between flex-column card-body">
+        <h5 class="card-title fw-bold text-center"></h5>
+        <p class="card-text"></p>
+        <div class="d-flex justify-content-between align-items-baseline">
+            <p class="card-text fw-bold">$</p>
+            <a class="btn-card fw-bold" href="./assets/pages/details.html">Ver más...</a>    
+            </div>
+    </div>
+</article>
+*/
+/* Elementos para trabajar
+    image
+    name
+    description
+    price
+ */
---
+++ assets/js/past-events.js
@@ -0,0 +1,38 @@
+// variables
+const contenedor = document.getElementById("eventos")
+const eventos = filtrarEventos (data.eventos)
+let card = ``
+
+// funcion para crear el article
+function agregarEventoHtml(evento) {
+    // variable para crear un alt usando la variable name del objeto
+    let alt = evento.name.replace(` `,`-`)
+    return `<article class="card bg-black col-10 col-md-3 rounded-5">
+                <img class="h-50 p-2 rounded-5" src="${evento.image}" class="card-img-top" alt="${alt}">
+                <div class="d-flex justify-content-between flex-column card-body">
+                    <h5 class="card-title fw-bold text-center">${evento.name}</h5>
+                    <p class="card-text">${evento.description}</p>
+                    <div class="d-flex justify-content-between align-items-baseline">
+                        <p class="card-text fw-bold">$${evento.price}</p>
+                        <a class="btn-card fw-bold" href="./details.html">Ver más...</a>    
+                    </div>
+                </div>
+            </article>`
+}
+
+// funcion para filtrar por fecha
+function filtrarEventos(arrayEventos) {
+    const eventosFiltrados = []
+    for (let evento of arrayEventos) {
+        if (evento.date <= '2022-01-01') { 
+        eventosFiltrados.push(evento)
+        }
+    }
+    return eventosFiltrados
+}
+
+// funcion para mostrar en la pantalla
+for (let evento of eventos) {
+    card += agregarEventoHtml(evento)
+}
+contenedor.innerHTML = card; 
