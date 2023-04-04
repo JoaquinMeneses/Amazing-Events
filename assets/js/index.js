e293a16131a19cddf195f633bcf903168f35076d
“JoaquinMeneses” <joakinmeneses.jm@gmail.com>
2023-04-04 12:05:32 -0300

    task3-v1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,24 @@
-// Trabajo con innerHTML
-const contenedor = document.getElementById("eventos")
-const eventos = data.eventos
-const evento = eventos
-let card = ``
+// variables
+const contenedor = document.getElementById("eventos");
+const eventos = data.eventos;
+let card = eventos.map(agregarEvento).join(" ");
 
-function agregarEventoHtml(evento) {
-    let alt = evento.name.replace(/\s/g,`-`)
-    return `<article class="card bg-black col-10 col-md-3 rounded-5">
+// funcion para crear el article
+function agregarEvento(evento) {
+    const alt = evento.name.replace(/\s/g, `-`).toLowerCase();
+    return `<article class="card bg-black col-10 col-md-3 height1 rounded-5">
                 <img class="h-50 p-2 rounded-5" src="${evento.image}" class="card-img-top" alt="${alt}">
                 <div class="d-flex justify-content-between flex-column card-body">
                     <h5 class="card-title fw-bold text-center">${evento.name}</h5>
                     <p class="card-text">${evento.description}</p>
                     <div class="d-flex justify-content-between align-items-baseline">
                         <p class="card-text fw-bold m-0">$${evento.price}</p>
-                        <a class="btn-card fw-bold" href="./assets/pages/details.html">See more...</a>    
+                        <a class="btn-card fw-bold" href="./assets/pages/details.html">See more...</a>
                     </div>
                 </div>
-            </article>`
+            </article>`;
 }
 
-for (let eventoHTML of eventos) {
-    card += agregarEventoHtml(eventoHTML)
-}
 contenedor.innerHTML = card;
 
 /* Plantilla de card
--- assets/js/past-events.js
+++ assets/js/past-events.js
@@ -1,39 +1,29 @@
 // variables
-const contenedor = document.getElementById("eventos")
-const eventos = filtrarEventos (data.eventos)
-let card = ``
+const contenedor = document.getElementById("eventos");
+const eventos = filtrarEventos(data.eventos);
+let card = eventos.map(agregarEvento).join(" ");
 
 // funcion para crear el article
-function agregarEventoHTML(evento) {
-    // variable para crear un alt usando la variable name del objeto
-    let alt = evento.name.replace(/\s/g,`-`)
-    return `<article class="card bg-black col-10 col-md-3 rounded-5">
+function agregarEvento(evento) {
+    const alt = evento.name.replace(/\s/g, `-`).toLowerCase();
+    return `<article class="card bg-black col-10 col-md-3 height1 rounded-5">
                 <img class="h-50 p-2 rounded-5" src="${evento.image}" class="card-img-top" alt="${alt}">
                 <div class="d-flex justify-content-between flex-column card-body">
                     <h5 class="card-title fw-bold text-center">${evento.name}</h5>
                     <p class="card-text">${evento.description}</p>
                     <div class="d-flex justify-content-between align-items-baseline">
                         <p class="card-text fw-bold m-0">$${evento.price}</p>
-                        <a class="btn-card fw-bold" href="./details.html">See more...</a>    
+                        <a class="btn-card fw-bold" href="./assets/pages/details.html">See more...</a>
                     </div>
                 </div>
-            </article>`
+            </article>`;
 }
 
 // funcion para filtrar por fecha
 function filtrarEventos(arrayEventos) {
-    const fechaFiltro = data.fechaActual
-    const eventosFiltrados = []
-    for (let evento of arrayEventos) {
-        if (evento.date < fechaFiltro) { 
-            eventosFiltrados.push(evento)
-        }
-    }
-    return eventosFiltrados
+    const fechaFiltro = data.fechaActual;
+    return arrayEventos.filter((evento) => evento.date < fechaFiltro);
 }
 
-// para mostrar en la pantalla
-for (let eventoHTML of eventos) {
-    card += agregarEventoHTML(eventoHTML)
-}
-contenedor.innerHTML = card; 
+// mostrar en la pantalla
+contenedor.innerHTML = card;
