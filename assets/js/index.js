fdefc9eadaa7ea6a41b05ef158cd4a6fae2f17d8
“JoaquinMeneses” <joakinmeneses.jm@gmail.com>
2023-04-04 10:49:25 -0300

    task2-final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 // Trabajo con innerHTML
-const contenedor = document.getElementById("eventos");
-const eventos = data.eventos;
+const contenedor = document.getElementById("eventos")
+const eventos = data.eventos
+const evento = eventos
+let card = ``
 
-function agregarEvento(evento) {
-    const alt = evento.name.replace(/\s/g, `-`);
+function agregarEventoHtml(evento) {
+    let alt = evento.name.replace(/\s/g,`-`)
     return `<article class="card bg-black col-10 col-md-3 rounded-5">
                 <img class="h-50 p-2 rounded-5" src="${evento.image}" class="card-img-top" alt="${alt}">
                 <div class="d-flex justify-content-between flex-column card-body">
@@ -11,15 +13,16 @@ function agregarEvento(evento) {
                     <p class="card-text">${evento.description}</p>
                     <div class="d-flex justify-content-between align-items-baseline">
                         <p class="card-text fw-bold m-0">$${evento.price}</p>
-                        <a class="btn-card fw-bold" href="./assets/pages/details.html">See more...</a>
+                        <a class="btn-card fw-bold" href="./assets/pages/details.html">See more...</a>    
                     </div>
                 </div>
-            </article>`;
+            </article>`
 }
 
-const card = eventos.map(agregarEvento).join("");
+for (let eventoHTML of eventos) {
+    card += agregarEventoHtml(eventoHTML)
+}
 contenedor.innerHTML = card;
-
 
 /* Plantilla de card
 <article class="card bg-black col-10 col-md-3 rounded-5">
