f5c478a87be8e28ac819c09b8f71043aced310b8
“JoaquinMeneses” <joakinmeneses.jm@gmail.com>
2023-04-06 12:40:38 -0300

    task3-v1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
-// variables
-const contenedor = document.getElementById("eventos");
+// variables de eventos
+const contenedorEventos = document.getElementById("eventos");
 const eventos = data.eventos;
-let card = eventos.map(agregarEvento).join(" ");
+let plantillaEvento = eventos.map(agregarEvento).join(" ");
 
-// funcion para crear el article
+// funcion para crear el evento
 function agregarEvento(evento) {
     const alt = evento.name.replace(/\s/g, `-`).toLowerCase();
     return `<article class="card bg-black col-10 col-md-3 height1 rounded-5">
@@ -19,24 +19,46 @@ function agregarEvento(evento) {
             </article>`;
 }
 
-contenedor.innerHTML = card;
+// mostrar en la pantalla
+contenedorEventos.innerHTML = plantillaEvento;
 
-/* Plantilla de card
-<article class="card bg-black col-10 col-md-3 rounded-5">
-    <img class="h-50 p-2 rounded-5" src="" class="card-img-top" alt="">
-    <div class="d-flex justify-content-between flex-column card-body">
-        <h5 class="card-title fw-bold text-center"></h5>
-        <p class="card-text"></p>
-        <div class="d-flex justify-content-between align-items-baseline">
-            <p class="card-text fw-bold">$</p>
-            <a class="btn-card fw-bold" href="./assets/pages/details.html">Ver más...</a>    
-            </div>
-    </div>
-</article>
-*/
-/* Elementos para trabajar
-    image
-    name
-    description
-    price
- */
+// variables de categorias
+const contenedorCategorias = document.getElementById("categorias");
+const categorias = data.eventos;
+const categoriasNoRepetidas = new Set();
+let plantillaCategoria = "";
+let idCategoria = 1; // variable para id y for de los elementos
+
+// crear etiquetas
+categorias.forEach((evento) => {
+    if (!categoriasNoRepetidas.has(evento.category)) {
+        // si la categoría no está en el Set, agregarla y añadir su plantilla
+        categoriasNoRepetidas.add(evento.category);
+        plantillaCategoria += `<div class="m-2">
+                            <input class="checkbox-categoria" onChange="filtrarPorCategorias()" type="checkbox" name="${evento.name}" value="${evento.category}" id="categoria-${idCategoria}" checked: "">
+                            <label for="categoria-${idCategoria}">${evento.category}</label>
+                        </div>`;
+
+        idCategoria++; // incrementar el valor de idCategoria
+    }
+});
+
+// mostrar en la pantalla
+contenedorCategorias.innerHTML = plantillaCategoria;
+
+function filtrarPorCategorias() {
+    const checkboxesCategorias = document.getElementsByClassName("checkbox-categoria");
+    const categoriasSeleccionadas = [];
+    for (let checkbox of checkboxesCategorias) {
+        if (checkbox.checked) {
+            categoriasSeleccionadas.push(checkbox.value);
+        }
+    }
+    if (categoriasSeleccionadas.length === 0) {
+        contenedorEventos.innerHTML = plantillaEvento;
+    } else {
+        const eventosFiltrados = eventos.filter(evento => categoriasSeleccionadas.includes(evento.category));
+        const plantillaFiltrada = eventosFiltrados.map(agregarEvento).join(" ");
+        contenedorEventos.innerHTML = plantillaFiltrada;
+    }
+}
