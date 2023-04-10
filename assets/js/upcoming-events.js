89b1a8a0477a0ff95b1603e4c6f7f4a27674f3c2
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-10 16:40:24 -0300

    task4-v1

--- assets/js/upcoming-events.js
+++ assets/js/upcoming-events.js
@@ -1,96 +1,94 @@
-// variables
-const contenedorEventos = document.getElementById("eventos");
-const eventos = data.eventos;
-let plantillaEvento = eventos.map(agregarEvento).join(" ");
+const eventos = data.eventos
 
-// funcion para crear el article
-function agregarEvento(evento) {
-    const alt = evento.name.replace(/\s/g, `-`).toLowerCase();
-    const fechaFiltro = data.fechaActual;
-    if (evento.date > fechaFiltro){
-        return `<article class="card bg-black col-10 col-md-3 height1 rounded-5">
-                <img class="h-50 p-2 rounded-5" src="${evento.image}" class="card-img-top" alt="${alt}">
-                <div class="d-flex justify-content-between flex-column card-body">
-                    <h5 class="card-title fw-bold text-center">${evento.name}</h5>
-                    <p class="card-text">${evento.description}</p>
-                    <div class="d-flex justify-content-between align-items-baseline">
-                        <p class="card-text fw-bold m-0">$${evento.price}</p>
-                        <a class="btn-card fw-bold" href="../pages/details.html?id=${evento.name}">See more...</a>
+const contenedorCartas = document.getElementById("eventos")
+
+let plantillaEventos = ""
+
+function agregarEvento (eventos){
+    if (eventos.date > data.fechaActual){
+        const alt = eventos.name.replace(/\s/g, `-`).toLowerCase();
+        return `<div class="card bg-black col-10 col-md-3 height1 rounded-5">
+                    <img class="h-50 p-2 rounded-5" src="${eventos.image}" class="card-img-top" alt="${alt}">
+                    <div class="d-flex justify-content-between flex-column card-body">
+                        <h5 class="card-title fw-bold text-center">${eventos.name}</h5>
+                        <p class="card-text">${eventos.description}</p>
+                        <div class="d-flex justify-content-between align-items-baseline">
+                            <p class="card-text fw-bold m-0">$${eventos.price}</p>
+                            <a class="btn-card fw-bold" href="../pages/details.html?id=${eventos.name}">See more...</a>
+                        </div>
                     </div>
-                </div>
-            </article>`;
-    }
-}
-
-// mostrar en la pantalla
-contenedorEventos.innerHTML = plantillaEvento;
-
-// variables filtro por categoria
-const contenedorCategorias = document.getElementById("categorias");
-const categorias = data.eventos;
-let plantillaCategoria = "";
-let idCategoria = 1; // variable para id y for
-const categoriasNoRepetidas = new Set();
-categorias.forEach((evento) => {
-    if (!categoriasNoRepetidas.has(evento.category)) {
-        // si la categoría no está en el Set, agregarla y añadir su plantilla
-        categoriasNoRepetidas.add(evento.category);
-        plantillaCategoria += `<div class="m-2">
-                            <input class="checkbox-categoria" onChange="filtrarPorCategorias()" type="checkbox" name="${evento.name}" value="${evento.category}" id="categoria-${idCategoria}" checked: "">
-                            <label for="categoria-${idCategoria}">${evento.category}</label>
-                        </div>`;
-        idCategoria++; // aumenta el valor de idCategoria
-    }
-});
-
-// funcion filtro por categoria
-function filtrarPorCategorias() {
-    const checkboxesCategorias = document.getElementsByClassName("checkbox-categoria");
-    const categoriasSeleccionadas = [];
-    for (let checkbox of checkboxesCategorias) {
-        if (checkbox.checked) {
-        categoriasSeleccionadas.push(checkbox.value);
-        }
-    }
-    const textoBuscado = buscador.value.toLowerCase(); // se agrega el valor del buscador
-    if (categoriasSeleccionadas.length === 0 && textoBuscado === "") { // si no hay categorías seleccionadas y el buscador está vacío, mostrar todos los eventos
-        contenedorEventos.innerHTML = plantillaEvento;
+                </div>`;
     } else {
-        const eventosFiltradosPorCategoria = categoriasSeleccionadas.length === 0 ? eventos : eventos.filter(evento => categoriasSeleccionadas.includes(evento.category));
-      // si no hay categorías seleccionadas muestra todos los eventos de lo contrario filtrar los eventos por categoría seleccionada.
-        const eventosFiltradosPorTexto = eventosFiltradosPorCategoria.filter(evento => evento.name.toLowerCase().includes(textoBuscado) || evento.description.toLowerCase().includes(textoBuscado));
-      // filtrar los eventos filtrados por categoría por el texto buscado
-        const plantillaFiltrada = eventosFiltradosPorTexto.map(agregarEvento).join(" ");
-        contenedorEventos.innerHTML = plantillaFiltrada;
-        if (eventosFiltrados.length === 0) {
-            contenedorEventos.innerHTML = "<p>No hay eventos que coincidan con los filtros seleccionados.</p>";
-        }
+        return '';
     }
 }
-// mostrar en la pantalla
-contenedorCategorias.innerHTML = plantillaCategoria;
 
-// variable filtro por buscador
-const buscador = document.getElementById("buscador");
-buscador.addEventListener("input", filtrarPorTexto);
+for (let evento of eventos){
+    plantillaEventos += agregarEvento(evento); 
+}
 
-// funcion filtrar por buscador
-function filtrarPorTexto() {
-    const textoBuscado = buscador.value.toLowerCase();
-    const checkboxesCategorias = document.getElementsByClassName("checkbox-categoria");
-    const categoriasSeleccionadas = [];
-    for (let checkbox of checkboxesCategorias) {
-        if (checkbox.checked) {
-            categoriasSeleccionadas.push(checkbox.value);
-        }
+contenedorCartas.innerHTML = plantillaEventos
+let buscador = document.getElementById("buscador")
+let contenedorCategorias = document.getElementById("contenedorCategorias")
+
+let categorias = eventos.map (evento => evento.category)
+/* console.log(categorias) */
+let categoriasFiltrado = new Set (categorias)
+let categoriasFinal = Array.from(categoriasFiltrado)
+/* console.log(categoriasFinal) */
+
+imprimirCategorias(categoriasFinal, contenedorCategorias)
+
+function imprimirCategorias(array, contenedor){
+    let plantillaCategorias = ""
+    for (let categoria of array){
+        plantillaCategorias += `<div class="d-flex align-items-center">
+                        <input class="form-check-input m-2" type="checkbox" name="${categoria}" value="${categoria}">
+                        <label class="form-check-label m-2" for="${categoria}">${categoria}</label>
+                    </div>`
     }
-    const eventosFiltrados = eventos.filter(evento => 
-        (evento.name.toLowerCase().includes(textoBuscado) || evento.description.toLowerCase().includes(textoBuscado)) &&
-        (categoriasSeleccionadas.length === 0 || categoriasSeleccionadas.includes(evento.category))
-    );
-    const plantillaFiltrada = eventosFiltrados.map(agregarEvento).join(" ");
-    contenedorEventos.innerHTML = plantillaFiltrada;
-    if (eventosFiltrados.length === 0) {
-        contenedorEventos.innerHTML = "<p>No hay eventos que coincidan con los filtros seleccionados.</p>";
+    contenedor.innerHTML = plantillaCategorias
+    
+}
+
+contenedorCategorias.addEventListener("change", (e) => {
+    let arrayCategorias = Array.from( document.querySelectorAll('input[type="checkbox"]:checked') ).map(cat => cat.name)
+    /* console.log(arrayCategorias) */
+    let filtro = filtrarPorCategoria(eventos, arrayCategorias)
+    let resultados = buscadorDeTexto(filtro, buscador.value)
+    imprimirEventos(resultados)
+})
+
+buscador.addEventListener("input", (e) => {
+    let arrayCategorias = Array.from( document.querySelectorAll('input[type="checkbox"]:checked') ).map(cat => cat.name)
+    /* console.log(arrayCategorias) */
+    let filtro = filtrarPorCategoria(eventos, arrayCategorias)
+    let resultados = buscadorDeTexto(filtro, buscador.value)
+    imprimirEventos(resultados)
+})
+
+function filtrarPorCategoria(array, categorias){
+    if ( categorias.length === 0 ){
+        return array
+    }else{
+        return array.filter( array => categorias.includes(array.category) );
+    }
+}
+
+function buscadorDeTexto(array, texto){
+    if (!texto){
+        return array;
+    }else{
+        let textoMin = texto.toLowerCase();
+        return array.filter( nota => nota.name.toLowerCase().includes(textoMin) || nota.description.toLowerCase().includes(textoMin) )
+    }
+}
+
+function imprimirEventos(parametro){
+    if (parametro.length === 0){
+        contenedorCartas.innerHTML = "There are no events to show";
+    }else{
+        let nota = parametro.map(agregarEvento).join(" ")
+        contenedorCartas.innerHTML = nota;
     }
 }
